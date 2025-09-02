22625238a356623579ab8daca60b9678554179b2
annabui9 <annabui1009@gmail.com>
2025-09-02 12:18:44 -0500

    added constraints, created string resources, updated ids

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -1,6 +1,8 @@
 package com.example.repairorder;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class RepairOrderActivity extends AppCompatActivity { // parent class for android activities
+
+    double numbers = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // this is what is created when launched
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_repair_order); //UI laid out by this line
@@ -20,5 +27,14 @@ public class RepairOrderActivity extends AppCompatActivity { // parent class for
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+//        EditText orderTypeET = findViewById(R.id.editOrderType);
+//        Random gen = new Random();
+//        double number = gen.nextDouble();
+//        String n = "$" + number;
+//        //orderTypeET.setText(R.string.);
+//        String value = orderTypeET.getText().toString();
+//        Log.i("TEST", value);
+
     }
 }
