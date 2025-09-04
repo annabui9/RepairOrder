7587a4de04ec44d3ef8412ec6b3f4a0364d666bd
annabui9 <annabui1009@gmail.com>
2025-09-04 11:44:01 -0500

    submit button assigns the subtotal a random number

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -2,6 +2,8 @@ package com.example.repairorder;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,6 +19,19 @@ public class RepairOrderActivity extends AppCompatActivity { // parent class for
 
     double numbers = 0.0;
 
+    TextView subtotalPrice;
+    Button submitB; // 1. create button
+
+    View.OnClickListener buttonListener = new View.OnClickListener() { // 3. create listener
+        @Override
+        public void onClick(View v) { //5.
+            Random gen = new Random();
+            double number = gen.nextDouble();
+            String n = "$" + number;
+            subtotalPrice.setText(n);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) { // this is what is created when launched
 
@@ -29,13 +44,16 @@ public class RepairOrderActivity extends AppCompatActivity { // parent class for
             return insets;
         });
 
-        TextView subtotalPrice= findViewById(R.id.subtotalValue);
-        Random gen = new Random();
-        double number = gen.nextDouble();
-        String n = "$" + number;
-        subtotalPrice.setText(n); //UI
-        String value = subtotalPrice.getText().toString(); //What's in the UI into the code
-        Log.i("TEST", value);
+        subtotalPrice= findViewById(R.id.subtotalValue); //locate any thing in UI
+        submitB = findViewById(R.id.submitButton); // 2.
+        submitB.setOnClickListener(buttonListener); // 4. registering the listener to the button
+
+//        Random gen = new Random();
+//        double number = gen.nextDouble();
+//        String n = "$" + number;
+//        subtotalPrice.setText(n); //UI
+//        String value = subtotalPrice.getText().toString(); //What's in the UI into the code
+//        Log.i("TEST", value);
 
     }
 }
